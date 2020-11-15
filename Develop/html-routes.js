f2cde62edeb43113e0bcc00119fcbcbffd69ddf0
Sam Scibetta <sammieeccentric@gmail.com>
2020-11-15 12:58:46 -0500

    added a separarte file for html routes.

---
+++ Develop/html-routes.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+module.exports = function(app) {
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+}
