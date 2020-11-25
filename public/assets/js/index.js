d464be2c4728e67b400f4b4217b5155cfd22ba76
Sam Scibetta <sammieeccentric@gmail.com>
2020-11-25 11:04:49 -0500

    this code was working on monday, but today it is broken even though I didn't change anything

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -55,8 +55,9 @@ const handleNoteSave = function () {
     title: $noteTitle.val(),
     text: $noteText.val(),
   };
-
+console.log("is it saving?")
   saveNote(newNote).then(() => {
+    console.log("hey its running")
     getAndRenderNotes();
     renderActiveNote();
   });
--- routes.js
+++ routes.js
@@ -4,21 +4,32 @@ const fs = require("fs")
 const { v4: uuidv4 } = require('uuid'); uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
 module.exports = function (app) {
+
+  app.use(express.static(__dirname + '/public'));
   // Basic route that sends the user first to the AJAX Page
-  app.all("/api/notes", function (req, res) {
-
+  app.get("/api/notes", function (req, res) {
+    console.log("get")
     res.sendFile(path.join(__dirname, "db/db.json"));
+  });
 
-/*     const newTitle = req.body.title;
-    const newText = req.body.text;
-    console.log(newText)
-    const newNotes = { newTitle, newText, id: uuidv4 }
-    notes.push(newNotes);
-    let note = JSON.stringify(notes); 
+  app.post("/api/notes", function (req, res) {
+    console.log("post")
+    //res.sendFile(path.join(__dirname, "db/db.json"));
+    fs.readFile('db/db.json', 'utf8', function (err, data) {
+      const notes = JSON.parse(data)
+      console.log(notes)
+      const title = req.body.title;
+      const text = req.body.text;
+      console.log(text)
+      const newNotes = { title, text, id: uuidv4 }
+      notes.push(newNotes);
+      let note = JSON.stringify(notes);
+      fs.writeFile("db/db.json", note, function () {
+        res.end()
+      })
+    })
 
-    fs.writeFile("db/db.json", note, function () {
-      return newNote;
-    })  */
+    
 
   });
 
@@ -31,6 +42,6 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
   });
 
-  app.use(express.static(__dirname + '/public'));
+
 
 }
